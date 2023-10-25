7ea829014dad4667e9d7bb30d20c375c918e783e
Костенков Даниил Дмитриевич <78727203+shlyapp@users.noreply.github.com>
2023-10-25 17:21:12 +0400

    Page Navigation

---
+++ ViewModel/PageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentApp.ViewModel
+{
+    public class PageViewModel : ViewModelBase
+    {
+        private string _label;
+
+        public string Label
+        {
+            get
+            {
+                return _label;
+            }
+            set
+            {
+                _label = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
