8cb948cf86811747939b12eac493a18e10b36497
Костенков Даниил Дмитриевич <78727203+shlyapp@users.noreply.github.com>
2023-10-25 21:54:34 +0400

    Example of read excel file

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TournamentApp.Utitlities;
 
 namespace TournamentApp
 {
@@ -20,6 +21,7 @@ namespace TournamentApp
         public MainWindow()
         {
             InitializeComponent();
+            ExcelReader.GetListUsers("example.xlsx");
         }
     }
 }
---
+++ Utitlities/ExcelReader.cs
@@ -0,0 +1,42 @@
+﻿using Aspose.Cells;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TournamentApp.Model;
+
+namespace TournamentApp.Utitlities
+{
+    public static class ExcelReader
+    {
+        public static List<User> GetListUsers(string path)
+        {
+            Workbook workbook = new Workbook(path);
+            Worksheet worksheet = workbook.Worksheets[0];
+
+            List<User> users = new List<User>();
+
+            for (int i = 1; i < worksheet.Cells.MaxDataRow; i++)
+            {
+                User user = new User(
+                    (int)worksheet.Cells[i, 0].Value,
+                    (string)worksheet.Cells[i, 1].Value,
+                    (string)worksheet.Cells[i, 2].Value,
+                    (DateTime)worksheet.Cells[i, 3].Value,
+                    (int)worksheet.Cells[i, 4].Value,
+                    (int)worksheet.Cells[i, 5].Value,
+                    (string)worksheet.Cells[i, 6].Value,
+                    (string)worksheet.Cells[i, 7].Value,
+                    (string)worksheet.Cells[i, 8].Value
+                    );
+
+                users.Add(user);
+            }
+
+            return users;
+        }
+    }
+}
