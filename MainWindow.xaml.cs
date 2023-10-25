242338d590d981efb42913176233a8393749d6d8
Костенков Даниил Дмитриевич <78727203+shlyapp@users.noreply.github.com>
2023-10-25 15:29:28 +0400

    Edit namespace

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,11 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace TournamentApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
