a2a95e5d2227be585b454cb096287cca6ff1428d
PadhiiiY <126673974+PadhiiiY@users.noreply.github.com>
2023-11-19 17:00:32 +0400

    Calculator
    Добавил таблицу
    Портировал данные из таблицы в калькулятор

--- ViewModel/CalculatorViewModel.cs
+++ ViewModel/CalculatorViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using Aspose.Cells.Drawing;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using TournamentApp.Model;
 
 namespace TournamentApp.ViewModel
 {
@@ -14,7 +16,13 @@ namespace TournamentApp.ViewModel
         {
             Label = "Калькулятор Ката";
 
+            foreach(Participant participant in Database.Participants.Elements)
+            {
+                Participants.Add(participant);
+            }
+
         }
 
+        public ObservableCollection<Participant> Participants { get; set; } = new ObservableCollection<Participant>();
     }
 }
