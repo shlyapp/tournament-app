8b1c4dd29dad3cb9daaa24a646300e856a199e5b
Костенков Даниил Дмитриевич <78727203+shlyapp@users.noreply.github.com>
2023-11-19 20:26:48 +0300

    Example of search engine

--- ViewModel/TableViewModel.cs
+++ ViewModel/TableViewModel.cs
@@ -5,15 +5,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
+using System.Xml.Serialization;
 using TournamentApp.Model;
+using TournamentApp.Utilities;
 
 namespace TournamentApp.ViewModel
 {
     public class TableViewModel : PageViewModel
     {
+        private string _textForSearch = "";
+
         public TableViewModel()
         {
             Label = "Таблица участников";
+
+            SearchCommand = new RelayCommand(Search);
 
             foreach (Participant participant in Database.Participants.Elements)
             {
@@ -23,5 +30,36 @@ namespace TournamentApp.ViewModel
         }
 
         public ObservableCollection<Participant> Participants { get; set; } = new ObservableCollection<Participant>();
+
+        public ICommand SearchCommand { get; set; }
+
+        public string TextForSearch
+        {
+            get
+            {
+                return _textForSearch;
+            }
+            set
+            {
+                _textForSearch = value;
+            }
+        }
+
+        public void Search(object obj)
+        {
+            ObservableCollection<Participant> result = new ObservableCollection<Participant>();
+            foreach(Participant participant in Participants)
+            {
+                if (participant.Name.Contains(TextForSearch))
+                {
+                    result.Add(participant);
+                }
+
+                Participants = result;
+                OnPropertyChanged(nameof(Participants));
+            } 
+        }
     }
+
+
 }
